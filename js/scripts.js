320c69ca1d000444c8512dd07e44a2bd7f2c5f5c
EugeneZnm <eugenenzioki@gmail.com>
2018-07-13 18:52:48 +0300

    appending result to list

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 //BUSINESS END LOGIC
+var player;
 
 function play() {
     for (var y = 0; y <= newNumber; y++) {
         if ((y % 3 === 0) && (y % 5 === 0)) {
-            $("#result").append("ping pong");
+            $("#result").append("<li>ping pong</li>");
         } else if (y % 3 === 0) {
-            $("#result").append("ping");
+            $("#result").append("<li>ping</li>");
         } else if (y % 5 === 0) {
-            $("#result").append("pong");
+            $("#result").append("<li>pong</li>");
         } else {
-            $("#result").append(y);
+            $("#result").append("<li>y</li>");
         }
     }
 
